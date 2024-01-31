e3a829b22cfb45b8cc71de3af75a3744ba277666
phonegor95 <phoenixhung2@gmail.com>
2024-01-31 10:39:11 +0800

    Neat clinvar_summary_txt2hail.py

--- clinvar_summary_txt2hail.py
+++ clinvar_summary_txt2hail.py
@@ -28,13 +28,13 @@ def get_header_line(gzfile):
             raise ValueError("No header line found in the file")
     
 # Define a custom aggregation function
-def custom_agg_review_status(series):
+""" def custom_agg_review_status(series):
     if 'practice guideline' in series.values:
         return ['practice guideline']
     elif 'reviewed by expert panel' in series.values:
         return ['reviewed by expert panel']
     else:
-        return list(series)
+        return list(series) """
 
 def filter_review_status_to_priority(df):
     # Define the priority for each review status
@@ -83,140 +83,131 @@ def meets_review_criteria(review_status_list):
     # Return True if at least one of the criteria is met
     return has_single_submitter or has_one_practice_guideline or has_one_reviewed_by_expert_panel
 
-def process_genomic_data(work_dir, submission_summary, variant_summary, gnomad,
-                         ClinicalSignificance_Pathogenic, ClinicalSignificance_Benign,
-                         ReviewStatus_criteria, VariantType_filter, Phenotype_filter,
-                         Chromosome_criteria):
-    # Function implementation goes here
-    # You can add your data processing logic here, using the provided parameters
-    print("Processing genomic data with the following parameters:")
-    print(f"Work directory: {work_dir}")
-    # Add more print statements or actual processing logic as needed
-    pass
+def read_and_filter_variant_summary(file_path, chromosome_criteria, review_status_criteria, clinical_significance_pathogenic, variant_type_filter):
+    header_line, skip_rows = get_header_line(file_path)
+    df = pd.read_csv(file_path, sep='\t', skiprows=skip_rows, names=header_line, low_memory=False)
+    df = df[df['Assembly'] == 'GRCh38']
+    df = df[df['Chromosome'].isin(chromosome_criteria)]
+    df.replace({'Chromosome': {'MT': 'M'}}, inplace=True)
+    df['Chromosome'] = df['Chromosome'].apply(lambda x: 'chr' + str(x))
+    df = df[(df['PositionVCF'] != -1) & 
+            (df['ReferenceAlleleVCF'] != 'na') & 
+            (df['AlternateAlleleVCF'] != 'na')]
+    df = df[df['ReviewStatus'].isin(review_status_criteria)]
+    df = df[df['ClinicalSignificance'].str.contains('|'.join(clinical_significance_pathogenic), case=True, na=False)]
+    df = df[~df['Type'].isin(variant_type_filter)]
+    df = df.drop_duplicates(subset='VariationID', keep='first')
+    return df
 
-#work_dir = '/mnt/beegfs/hongyf/igenomes_base/Homo_sapiens/GATK/GRCh38/Annotation/ClinVar/'
-#submission_summary = work_dir + 'submission_summary_20240127.txt.gz'
-#variant_summary = work_dir + 'variation_allele_20240127.txt.gz'
-#gnomad = '/mnt/beegfs/hongyf/igenomes_base/Homo_sapiens/GATK/GRCh38/Annotation/GermlineResource/eas_af-only-gnomad.genomes.v4.0.sites.mt'
-#ClinicalSignificance_Pathogenic = ['Pathogenic', 'Likely pathogenic', 'Pathogenic, low penetrance', 'Likely pathogenic, low penetrance', 'Established risk allele', 'Likely risk allele']
-#ClinicalSignificance_Benign = ['Uncertain significance', 'Uncertain risk allele', 'Benign', 'Likely benign']
-#ReviewStatus_criteria = ['practice guideline', 'reviewed by expert panel', 'criteria provided, multiple submitters, no conflicts']
-#VariantType_filter = ['Complex', 'copy number loss', 'copy number gain', 'fusion', 'Inversion', 'Tandem duplication', 'Translocation']
-#Phenotype_filter = ['C3661900:not provided', 'CN169374:not specified', 'na:See cases']
-#Chromosome_criteria = [str(i) for i in range(1, 23)] + ['X', 'Y', 'MT']
+def read_and_filter_submission_summary(file_path, phenotype_filter):
+    header_line, skip_rows = get_header_line(file_path)
+    df = pd.read_csv(file_path, sep='\t', skiprows=skip_rows, names=header_line, low_memory=False)
+    df = df[~df['ReportedPhenotypeInfo'].isin(phenotype_filter)]
+    df = filter_review_status_to_priority(df)
+    return df
 
-# Create the parser
-parser = argparse.ArgumentParser(description='Process genomic data with specified filters and criteria.')
+def group_and_filter_submission_summary(df, clinical_significance_pathogenic, clinical_significance_benign):
+    df = df.groupby(['VariationID', 'ReportedPhenotypeInfo']).agg({
+        'ClinicalSignificance': list,
+        'Description': list,
+        'ReviewStatus': list,
+        'Submitter': list,
+        'SCV': list,
+        'ExplanationOfInterpretation': list
+    }).reset_index()
 
-# Define arguments that the script can accept
-parser.add_argument("--work_dir", default='/mnt/beegfs/hongyf/igenomes_base/Homo_sapiens/GATK/GRCh38/Annotation/ClinVar/', help="Working directory path")
-parser.add_argument("--submission_summary", default='submission_summary_20240127.txt.gz', help="Submission summary file")
-parser.add_argument("--variant_summary", default='variant_summary_20240127.txt.gz', help="Variant summary file")
-parser.add_argument("--date", default='20240127', help="Date of ClinVar file")
-parser.add_argument("--gnomad", default='/mnt/beegfs/hongyf/igenomes_base/Homo_sapiens/GATK/GRCh38/Annotation/GermlineResource/eas_af-only-gnomad.genomes.v4.0.sites.mt', help="GnomAD file path")
-parser.add_argument("--ClinicalSignificance_Pathogenic", type=str, default='Pathogenic,Likely pathogenic,Pathogenic low penetrance,Likely pathogenic low penetrance,Established risk allele,Likely risk allele', help="Clinical significance pathogenic categories")
-parser.add_argument("--ClinicalSignificance_Benign", type=str, default='Uncertain significance,Uncertain risk allele,Benign,Likely benign', help="Clinical significance benign categories")
-parser.add_argument("--ReviewStatus_criteria", type=str, default='practice guideline,reviewed by expert panel,criteria provided multiple submitters no conflicts', help="Review status criteria")
-parser.add_argument("--VariantType_filter", type=str, default='Complex,copy number loss,copy number gain,fusion,Inversion,Tandem duplication,Translocation', help="Variant type filters")
-parser.add_argument("--Phenotype_filter", type=str, default='C3661900:not provided,CN169374:not specified,na:See cases', help="Phenotype filters")
-parser.add_argument("--Chromosome_criteria", type=str, default=','.join([str(i) for i in range(1, 23)] + ['X', 'Y', 'MT']), help="Chromosome criteria")
+    # Subset the DataFrame based on the criteria
+    df = df[df['ClinicalSignificance'].apply(
+        lambda x: any(value in clinical_significance_pathogenic for value in x) and not any(value in clinical_significance_benign for value in x)
+    )].reset_index(drop=True)
 
-# Parse the arguments
-args = parser.parse_args()
+    df = df[df['ReviewStatus'].apply(meets_review_criteria)].reset_index(drop=True)
 
-work_dir = args.work_dir
-submission_summary = args.work_dir + args.submission_summary
-variant_summary = args.work_dir + args.variant_summary
-gnomad = args.gnomad
-date = args.date
+    # Group by 'VariationID' to collect all different 'ReportedPhenotypeInfo' entries
+    df = df.groupby('VariationID').agg({
+        'ReportedPhenotypeInfo': list,
+        'ClinicalSignificance': list,
+        'Description': list,
+        'ReviewStatus': list,
+        'Submitter': list,
+        'SCV': list,
+        'ExplanationOfInterpretation': list
+    }).reset_index()
+    return df
 
-# Split the comma-separated strings into lists
-ClinicalSignificance_Pathogenic = args.ClinicalSignificance_Pathogenic.split(',')
-ClinicalSignificance_Benign = args.ClinicalSignificance_Benign.split(',')
-ReviewStatus_criteria = args.ReviewStatus_criteria.split(',')
-VariantType_filter = args.VariantType_filter.split(',')
-Phenotype_filter = args.Phenotype_filter.split(',')
-Chromosome_criteria = args.Chromosome_criteria.split(',')
+def merge_and_prepare_for_hail(submission_df, variant_df):
+    merged_df = submission_df.merge(variant_df, on='VariationID')
+    ht = hl.Table.from_pandas(merged_df)
+    ht = ht.transmute(locus=hl.locus(ht.Chromosome, ht.PositionVCF, reference_genome='GRCh38'))
+    ht = ht.transmute(alleles=[ht.ReferenceAlleleVCF, ht.AlternateAlleleVCF])
+    ht = ht.key_by(ht.locus, ht.alleles)
+    return ht
 
+def annotate_with_gnomad(ht, gnomad_path):
+    gnomad_ht = hl.read_matrix_table(gnomad_path)
+    ht = ht.annotate(AF_global=gnomad_ht.rows()[ht.key].info.AF)
+    ht = ht.annotate(AF_eas=gnomad_ht.rows()[ht.key].info.AF_eas)
+    return ht
 
-header_line_variant, skip_rows_variant = get_header_line(variant_summary)
-variant_summary_df = pd.read_csv(variant_summary, sep='\t', comment=None, skiprows = skip_rows_variant, names = header_line_variant, low_memory=False)
-variant_summary_df = variant_summary_df[variant_summary_df['Assembly'] == 'GRCh38']
-variant_summary_df = variant_summary_df[variant_summary_df['Chromosome'].isin(Chromosome_criteria)]
-variant_summary_df.replace({'Chromosome': {'MT': 'M'}}, inplace=True)
-variant_summary_df['Chromosome'] = variant_summary_df['Chromosome'].apply(lambda x: 'chr' + str(x))
-variant_summary_df = variant_summary_df[variant_summary_df['PositionVCF'] != -1]
-variant_summary_df = variant_summary_df[variant_summary_df['ReferenceAlleleVCF'] != 'na']
-variant_summary_df = variant_summary_df[variant_summary_df['AlternateAlleleVCF'] != 'na']
-variant_summary_df = variant_summary_df[variant_summary_df['ReviewStatus'].isin(ReviewStatus_criteria)]
-variant_summary_df = variant_summary_df[variant_summary_df['ClinicalSignificance'].str.contains('|'.join(ClinicalSignificance_Pathogenic), case=True, na=False)]
-variant_summary_df = variant_summary_df[~variant_summary_df['Type'].isin(VariantType_filter)]
-variant_summary_df = variant_summary_df.drop_duplicates(subset='VariationID', keep='first')
-print(f"variant_summary_df: {variant_summary_df}")
+def main():
 
-header_line_submission, skip_rows_submission = get_header_line(submission_summary)
-submission_summary_df = pd.read_csv(submission_summary, sep='\t', comment=None, skiprows = skip_rows_submission, names = header_line_submission, low_memory=False)
-submission_summary_df = submission_summary_df[~submission_summary_df['ReportedPhenotypeInfo'].isin(Phenotype_filter)]
-submission_summary_df = filter_review_status_to_priority(submission_summary_df)
-
-# Step 1: Group by 'VariationID' and 'ReportedPhenotypeInfo' and aggregate the other columns
-submission_summary_df = submission_summary_df.groupby(['VariationID', 'ReportedPhenotypeInfo']).agg({
-    'ClinicalSignificance': lambda x: list(x),
-    #'DateLastEvaluated': lambda x: list(x),
-    'Description': lambda x: list(x),
-    #'SubmittedPhenotypeInfo': lambda x: x.value_counts().to_dict(),
-    'ReviewStatus': lambda x: list(x),
-    #'CollectionMethod': lambda x: x.value_counts().to_dict(),
-    #'OriginCounts': lambda x: x.value_counts().to_dict(),
-    'Submitter': lambda x: list(x),
-    'SCV': lambda x: list(x),
-    #'SubmittedGeneSymbol': lambda x: x.value_counts().to_dict(),
-    'ExplanationOfInterpretation': lambda x: list(x)
-}).reset_index()
-
-
-# Subset the DataFrame based on the criteria
-submission_summary_df = submission_summary_df[
-    submission_summary_df['ClinicalSignificance'].apply(
-    lambda x: any(value in ClinicalSignificance_Pathogenic for value in x) and not any(value in ClinicalSignificance_Benign for value in x)
+    variant_df = read_and_filter_variant_summary(
+        variant_summary, 
+        Chromosome_criteria, 
+        ReviewStatus_criteria, 
+        ClinicalSignificance_Pathogenic, 
+        VariantType_filter
     )
-].reset_index(drop=True)
+    print(f"variant_summary_df: {variant_df}")
 
-submission_summary_df = submission_summary_df[
-    submission_summary_df['ReviewStatus'].apply(meets_review_criteria)
-].reset_index(drop=True)
+    submission_df = read_and_filter_submission_summary(submission_summary, Phenotype_filter)
+    submission_df = group_and_filter_submission_summary(
+        submission_df, 
+        ClinicalSignificance_Pathogenic, 
+        ClinicalSignificance_Benign
+    )
+    print(f"submission_summary_df: {submission_df}")
 
-# Step 2: Group by 'VariationID' to collect all different 'ReportedPhenotypeInfo' entries
-submission_summary_df = submission_summary_df.groupby('VariationID').agg({
-    'ReportedPhenotypeInfo': lambda x: list(x),
-    'ClinicalSignificance': lambda x: list(x),
-    #'DateLastEvaluated': lambda x: list(x),
-    'Description': lambda x: list(x),
-    #'SubmittedPhenotypeInfo': lambda x: list(x),
-    'ReviewStatus': lambda x: list(x),
-    #'CollectionMethod': lambda x: list(x),
-    #'OriginCounts': lambda x: list(x),
-    'Submitter': lambda x: list(x),
-    'SCV': lambda x: list(x),
-    #'SubmittedGeneSymbol': lambda x: list(x),
-    'ExplanationOfInterpretation': lambda x: list(x)
-}).reset_index()
-print(f"variant_summary_df: {variant_summary_df}")
+    variant_submission_ht = merge_and_prepare_for_hail(submission_df, variant_df)
+    variant_submission_ht = annotate_with_gnomad(variant_submission_ht, gnomad)
+    variant_submission_ht.describe()
+    variant_submission_ht.show()
 
-variant_submission_summary_df = submission_summary_df.merge(variant_summary_df, on='VariationID')
-print(f"variant_submission_summary_df: {variant_submission_summary_df}")
+    variant_submission_ht.write(output_path, overwrite=True)
 
-# Step 3: Convert the final grouped DataFrame to a Hail Table
-variant_submission_summary_ht = hl.Table.from_pandas(variant_submission_summary_df[['VariationID', 'Name', 'Type', 'GeneSymbol', 'OriginSimple', 'Chromosome', 'PositionVCF', 'ReferenceAlleleVCF', 'AlternateAlleleVCF', 'ClinicalSignificance_y', 'ReviewStatus_y', 'ClinSigSimple', 'LastEvaluated', 'ReportedPhenotypeInfo', 'SCV', 'Submitter', 'Description', 'ExplanationOfInterpretation']])
-del variant_summary_df
-del submission_summary_df
-del variant_submission_summary_df
-variant_submission_summary_ht = variant_submission_summary_ht.transmute(locus = hl.locus(variant_submission_summary_ht.Chromosome, variant_submission_summary_ht.PositionVCF, reference_genome='GRCh38'))
-variant_submission_summary_ht = variant_submission_summary_ht.transmute(alleles = [variant_submission_summary_ht.ReferenceAlleleVCF, variant_submission_summary_ht.AlternateAlleleVCF])
-variant_submission_summary_ht = variant_submission_summary_ht.key_by(variant_submission_summary_ht.locus, variant_submission_summary_ht.alleles)
-gnomad_ht = hl.read_table(gnomad)
-variant_submission_summary_ht = variant_submission_summary_ht.annotate(AF_global = gnomad_ht.rows()[variant_submission_summary_ht.key].info.AF)
-variant_submission_summary_ht = variant_submission_summary_ht.annotate(AF_eas = gnomad_ht.rows()[variant_submission_summary_ht.key].info.AF_eas)
-variant_submission_summary_ht.describe()
-variant_submission_summary_ht.show()
-variant_submission_summary_ht.write(work_dir + 'clinvar_summary_' + date +'_w_gnomad.ht', overwrite=True)
+# Run the main function
+if __name__ == "__main__":
+    # Create the parser
+    parser = argparse.ArgumentParser(description='Process genomic data with specified filters and criteria.')
+
+    # Define arguments that the script can accept
+    parser.add_argument("--work_dir", default='/mnt/beegfs/hongyf/igenomes_base/Homo_sapiens/GATK/GRCh38/Annotation/ClinVar/', help="Working directory path")
+    parser.add_argument("--submission_summary", default='submission_summary_20240127.txt.gz', help="Submission summary file")
+    parser.add_argument("--variant_summary", default='variant_summary_20240127.txt.gz', help="Variant summary file")
+    parser.add_argument("--date", default='20240127', help="Date of ClinVar file")
+    parser.add_argument("--gnomad", default='/mnt/beegfs/hongyf/igenomes_base/Homo_sapiens/GATK/GRCh38/Annotation/GermlineResource/eas_af-only-gnomad.genomes.v4.0.sites.mt', help="GnomAD file path")
+    parser.add_argument("--ClinicalSignificance_Pathogenic", type=str, default='Pathogenic,Likely pathogenic,Pathogenic low penetrance,Likely pathogenic low penetrance,Established risk allele,Likely risk allele', help="Clinical significance pathogenic categories")
+    parser.add_argument("--ClinicalSignificance_Benign", type=str, default='Uncertain significance,Uncertain risk allele,Benign,Likely benign', help="Clinical significance benign categories")
+    parser.add_argument("--ReviewStatus_criteria", type=str, default='practice guideline,reviewed by expert panel,criteria provided multiple submitters no conflicts', help="Review status criteria")
+    parser.add_argument("--VariantType_filter", type=str, default='Complex,copy number loss,copy number gain,fusion,Inversion,Tandem duplication,Translocation', help="Variant type filters")
+    parser.add_argument("--Phenotype_filter", type=str, default='C3661900:not provided,CN169374:not specified,na:See cases', help="Phenotype filters")
+    parser.add_argument("--Chromosome_criteria", type=str, default=','.join([str(i) for i in range(1, 23)] + ['X', 'Y', 'MT']), help="Chromosome criteria")
+
+    # Define and parse command-line arguments here as shown previously
+    args = parser.parse_args()
+
+    work_dir = args.work_dir
+    submission_summary = args.work_dir + args.submission_summary
+    variant_summary = args.work_dir + args.variant_summary
+    gnomad = args.gnomad
+    date = args.date
+    output_path = work_dir + 'clinvar_summary_' + date + '_w_gnomad.ht'
+    
+    # Convert the comma-separated strings into lists
+    Chromosome_criteria = args.Chromosome_criteria.split(',')
+    ClinicalSignificance_Pathogenic = args.ClinicalSignificance_Pathogenic.split(',')
+    ClinicalSignificance_Benign = args.ClinicalSignificance_Benign.split(',')
+    ReviewStatus_criteria = args.ReviewStatus_criteria.split(',')
+    VariantType_filter = args.VariantType_filter.split(',')
+    Phenotype_filter = args.Phenotype_filter.split(',')
+    main()
